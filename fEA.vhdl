32992bd1b29e6067bc0796b126dc4aa1cedb2804
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-06-19 20:45:01 -0300

    Update fEA.vhdl

--- fEA.vhdl
+++ fEA.vhdl
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity f_ea is
+entity fEA is
     port(
         m25 : in std_logic;
         m50 : in std_logic;
@@ -11,7 +11,7 @@ entity f_ea is
     );
 end entity;
 
-architecture fEntrada_ea of f_ea is
+architecture fEntrada_ea of fEA is
     begin 
     pe(2) <= (m50 and m25) or (not ea(2) and ea(0) and m50) or (not(e(2)) and ea(0) and ea(1) and m25);
     pe(1) <= (not(ea(2)) and ea(1) and not(m25) or (e(1) and m25) or (m50 and m25) or (e(2) and m25));
