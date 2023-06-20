992151deab34638078898ae26cb1040334315ab4
Gabriel Jared <gabrieljaredjared@gmail.com>
2023-06-20 14:22:15 -0300

    faltando tb

---
+++ ME_chocolate.vhdl
@@ -0,0 +1,55 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity ME_chocolate is
+    port(
+        reset, clock : in std_logic;
+        m_25         : in std_logic;
+        m_50         : in std_logic;
+        out_dev      : out std_logic;
+        out_cof      : out std_logic;
+        out_disp     : out std_logic;
+    );
+end entity;
+
+architecture ME_choc of ME_chocolate is
+    component fEA is
+        port(
+            m25 : in std_logic;
+            m50 : in std_logic;
+            ea  : in bit_vector(2 downto 0);
+            pe  : out bit_vector(2 downto 0);
+        );
+    end component;
+    
+    component regis is
+        port(
+            din     : in std_logic_vector(2 downto 0);
+            cl, clk : in std_logic;
+            dout    : out std_logic_vector(2 downto 0)
+        );
+    end component;
+    
+    component sEA is
+        port(
+            ea     : in bit_vector(2 downto 0);
+            m_dev  : out std_logic;
+            m_cof  : out std_logic;
+            d_disp : out std_logic;
+        );
+    end component;
+
+    signal spe_reg, sea_reg  : std_logic_vector(2 downto 0);
+    signal sm25, sm50        : std_logic;
+    signal sdev, scof, sdisp : std_logic;
+
+    u_fcea : fEA port map (sm25, sm50, sea_reg, spe_reg);
+    u_reg  : regis port map(spe_reg, reset, clock, sea_reg);
+    u_fsea : sEA port map(spe_reg, sdev, scof, sdisp);
+
+	    out_dev <= sdev;
+        out_cof <= scof;
+        out_disp <= sdisp;
+end architecture;
+
--- fEA.vhdl
+++ fEA.vhdl
@@ -7,11 +7,12 @@ entity fEA is
         m25 : in std_logic;
         m50 : in std_logic;
         ea  : in bit_vector(2 downto 0);
-        pe  : in bit_vector(2 downto 0);
+        pe  : out bit_vector(2 downto 0);
     );
 end entity;
 
 architecture fEntrada_ea of fEA is
+
     begin 
     pe(2) <= (m50 and m25) or (not ea(2) and ea(0) and m50) or (not(e(2)) and ea(0) and ea(1) and m25);
     pe(1) <= (not(ea(2)) and ea(1) and not(m25) or (e(1) and m25) or (m50 and m25) or (e(2) and m25));
