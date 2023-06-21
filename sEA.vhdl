cc8957ce248bdaf6388c7db2498b7449ff064706
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2023-06-21 10:48:50 -0300

    Update sEA.vhdl

--- sEA.vhdl
+++ sEA.vhdl
@@ -4,16 +4,16 @@ use IEEE.numeric_std.all;
 
 entity sEA is
     port(
-        pe : in bit_vector(2 downto 0);
+        pe : in std_logic_vector(2 downto 0);
         m_dev : out std_logic;
         m_cof : out std_logic;
-        d_disp : out std_logic;
+        d_disp : out std_logic
     );
 end entity;
 
 architecture fsaida_ea of sEA is
     begin
-        m_dev <= e(2) and e(1);
-        m_cof <= e(2) and not e(1);
-        d_disp <= e(2) and not e(1);
+        m_dev <= pe(2) and pe(1);
+        m_cof <= pe(2) and not pe(1);
+        d_disp <= pe(2) and not pe(1);
 end architecture;
