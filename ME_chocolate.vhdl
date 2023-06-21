e9852ab9e4292a142e31c91ec54508651a30641b
JaredGabriel2001 <73358564+JaredGabriel2001@users.noreply.github.com>
2023-06-21 14:22:33 -0300

    faltando arq onda

--- ME_chocolate.vhdl
+++ ME_chocolate.vhdl
@@ -9,7 +9,7 @@ entity ME_chocolate is
         m_50         : in std_logic;
         out_dev      : out std_logic;
         out_cof      : out std_logic;
-        out_disp     : out std_logic;
+        out_disp     : out std_logic
     );
 end entity;
 
@@ -18,34 +18,36 @@ architecture ME_choc of ME_chocolate is
         port(
             m25 : in std_logic;
             m50 : in std_logic;
-            ea  : in bit_vector(2 downto 0);
-            pe  : out bit_vector(2 downto 0);
+            ea  : in std_logic_vector(2 downto 0);
+            pe  : out std_logic_vector(2 downto 0)
         );
     end component;
     
-    component regis is
+    component registrador is
         port(
-            din     : in std_logic_vector(2 downto 0);
+            per     : in std_logic_vector(2 downto 0);
             cl, clk : in std_logic;
-            dout    : out std_logic_vector(2 downto 0)
+            ear    : out std_logic_vector(2 downto 0)
         );
     end component;
     
     component sEA is
         port(
-            pe     : in bit_vector(2 downto 0);
+            pe     : in std_logic_vector(2 downto 0);
             m_dev  : out std_logic;
             m_cof  : out std_logic;
-            d_disp : out std_logic;
+            d_disp : out std_logic
         );
     end component;
 
     signal spe_reg, sea_reg  : std_logic_vector(2 downto 0);
     signal sm25, sm50        : std_logic;
     signal sdev, scof, sdisp : std_logic;
-
+    
+    begin
+        
     u_fcea : fEA port map (sm25, sm50, sea_reg, spe_reg);
-    u_reg  : regis port map(spe_reg, reset, clock, sea_reg);
+    u_reg  : registrador port map(spe_reg, reset,clock, sea_reg);
     u_fsea : sEA port map(spe_reg, sdev, scof, sdisp);
 
 	    out_dev <= sdev;
